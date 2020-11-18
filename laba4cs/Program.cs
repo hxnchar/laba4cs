f1d7dbe938aac50c2e72e343d356338f50a7d7dd
Honchar Oleksii <honcharaleksey@gmail.com>
2020-11-18 10:49:32 +0300

    Add project files.

---
+++ laba4cs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace laba4cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result=0, n, m;
+            Console.WriteLine("Input n..."); 
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input m...");
+            m = int.Parse(Console.ReadLine());
+            if(n>=Math.Pow(10, m - 1))
+            {
+                for (; m > 0; m--)
+                {
+                    result += n % 10;
+                    n /= 10;
+                }
+                Console.WriteLine($"Result is {result}.");
+            }
+            else
+            {
+                Console.WriteLine("M is greater than the number of digits of n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
